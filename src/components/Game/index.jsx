c256e51ab160bb3dba9c167816b0ea4c65750499
Julio Bonatti <julio.bonattif@gmail.com>
2025-01-28 00:31:28 -0300

    updated game section

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,11 +1,34 @@
 import "./styles.css";
+import { BoxPhiscs } from "./utils.mjs";
 import React, { useEffect, useState } from "react";
 
 function Game({ active }) {
+    const [boxXPos, setBoxXPos] = useState(0);
+    const [boxyPos, setBoxyPos] = useState(40);
+    const [boxMass, setBoxMass] = useState(1);
+    const [force, setForce] = useState(10);
+    const [gravity, setGravity] = useState(10);
+
+    const startGame = async () => {
+        const timeDelta = 50 //ms
+        const box = new BoxPhiscs(
+            boxXPos,
+            boxyPos,
+            boxMass,
+            force,
+            gravity,
+            timeDelta
+        );
+        do {
+            // aqui vou precisar fazer um generator function para pegar os valores
+        } while (box.calculateResultant.endScene === false);
+    }
+
+
     return (
         <div className="julios-game-interface-container">
             <div className="game-header">
-                <div className="game-button-start">
+                <div className="game-button-start" onClick={startGame}>
                     START
                 </div>
             </div>
--- src/components/Game/utils.mjs
+++ src/components/Game/utils.mjs
@@ -4,9 +4,10 @@ class Box {
         this.yPos = yPos;
         this.mass = mass;
         this.xAcel = 0;
+        this.xVel = 0;
         this.yVel = 0;
-        this.xAbsoluteP = 0;
-        this.yAbsoluteP = 0;
+        this.xAbsPos = 0;
+        this.yAbsPos = 0;
     };
 } 
 
@@ -16,21 +17,35 @@ class BoxPhisics extends Box {
 
         this.xForce = xForce;
         this.gravity = gravity;
-        this.timeDelta = timeDelta / 1000; // Convert timeDelta to seconds
+        this.timeDelta = timeDelta / 1000;
+
+        this.xVel = this.xForce
     }
     
-    calculateResultant () {
-        this.xAbsoluteP += this.xForce * this.timeDelta / this.mass;
-        this.yVel += this.gravity * this.mass * (this.timeDelta ** 2) / 2;
-        this.yAbsoluteP += this.yVel; 
+    checkFloorCollision () {
+        if (this.ypos >= 100) {
+            return true;
+        };
+        return false;
+    }
 
-        this.xPos = this.xAbsoluteP * 100;
-        this.yPos += this.yAbsoluteP * 100;
-        
-        return {
+    calculateResultant () {
+        this.xAbsPos += this.xForce * this.timeDelta / this.mass;
+        this.yVel += this.gravity * this.mass * (this.timeDelta ** 2) / 2;
+        this.yAbsPos += this.yVel; 
+
+        this.xPos = this.xAbsPos * 100;
+        this.yPos += this.yAbsPos * 100;
+
+        return (this.checkFloorCollision ? {
             xPos: this.xPos,
-            yPos: this.yPos
-        }
+            yPos: 100,
+            endScene: true // This actually needs to be ended wheen x and y velocity are 0
+        } : {
+            xPos: this.xPos,
+            yPos: this.yPos,
+            endScene: false
+        });
     }
 }
 
